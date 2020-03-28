253f8745e133dab1673254f83f3811b6bbcda39a
Filippo Volpe <filippovolpe98@gmail.com>
2020-03-28 16:53:13 +0100

    It sends and receives SPI datagit add .

--- up_and_run.sh
+++ up_and_run.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+set -e
 
+# import the configuration
 . server.conf
 
 REMOTE=$REMOTE_USER@$REMOTE_ADDRESS
 
+
 echo "Building..."
 
-# make all
-echo "make all"
+echo make all
+aarch64-linux-gnu-gcc RGB-ify.cpp -o build/RGB-ify
 
 echo "Loading to home server..."
 
 ssh $REMOTE "mkdir -p ~/bin"
 scp build/RGB-ify $REMOTE:~/bin/RGB-ify
-ssh $REMOTE "chmod +x ~/bin/RGB-ify && ~/bin/RGB-ify"
+ssh $REMOTE "chmod +x ~/bin/RGB-ify && ~/bin/RGB-ify /dev/spidev0.0"
 
